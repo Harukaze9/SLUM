4558282dbaec4288a93eec98ba4944fd81ca3e70
Harukaze8 <uminohiyoko@gmail.com>
2019-12-09 01:23:58 +0900

    test commit

---
+++ clipboard/clipboard.py
@@ -0,0 +1,15 @@
+class ClipBoardManager:
+    def __init__(self):
+        print("hello constructor")
+        self.kuma = 30
+
+    def kumap(self):
+        print("kuma is ", self.kuma)
+
+
+
+
+if __name__ == '__main__':
+    print("hwllo wooo")
+    hoge = ClipBoardManager()
+    hoge.kumap()
