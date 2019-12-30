23cfa652e7596c7935c4f5ac70b247c89e52eb69
Harukaze8 <uminohiyoko@gmail.com>
2019-12-31 02:18:30 +0900

    improved

--- clipboard/clipboard.py
+++ clipboard/clipboard.py
@@ -1,68 +1,140 @@
 import os
 import argparse
 import json
+import datetime
+import math
+import pandas as pd #こいつのimport だけで0.5[s] かかる (これを外すと実行時間を含めて0.03[s]くらい)
+import pyperclip
 
 
 class ClipBoardManager:
     main_dir          = os.path.dirname(os.path.abspath(__file__))
     data_dir = main_dir + "/data/"
     json_data = data_dir + 'content.json'
-    tmpdir       = data_dir + '.tmp'
 
     def __init__(self):
         self.data = self.read_data()
+        self.df = pd.DataFrame.from_dict(self.data, orient="index")
+        self.df.index.name="key"
 
     def read_data(self):
         if not os.path.exists(self.json_data):
             print("{} was not found".format(self.json_data))
             print("Creating {}".format(self.json_data))
             with open(self.json_data, "w") as f:
-                json_data = json.dumps({})
-                json.dump(json_data, f)
+                dic = self.get_initial_data()
+                # json_data = json.dumps(dic)
+                json.dump(dic, f)
+
         with open(self.json_data) as f:
             data = json.load(f)
             return data
 
-    def add_data(self, input_key, input_value):
-        self.data[input_key] = input_value
+    def add_data(self, add_key, add_content):
+        dt = datetime.datetime.now()
+        self.data[add_key] = {}
+        self.data[add_key]["content"] = add_content
+        self.data[add_key]["timestamp"] = math.floor(dt.timestamp())
         f = open(self.json_data, "w")
         json.dump(self.data, f)
+        print("Success: {} is added!".format(add_key))
 
-    def test_func(self):
-        print(self.tmpdir)
-
-    def show_contents(self):
-        print(self.data)
+    def show_contents(self, short=False):
+        if (len(self.df) == 0):
+            self.reset_contents()
+        print("=============== Database =====================")
+        if short:
+            print(list(self.df.sort_values("timestamp").index))
+        else:
+            print(self.df.sort_values("timestamp").head(n=10))
+        print("===============================================")
 
     def reset_contents(self):
         with open(self.json_data, "w") as f:
-            json.dump({}, f)
+            json.dump(self.get_initial_data(), f)
+        self.reload()
 
     def get_initial_data(self):
-        return {}
+        dic = { "example":{
+                    "content": "\"this is an example string\"",
+                    "timestamp": 0
+                    }
+                }
+        return dic
+
+    def reload(self):
+        self.data = self.read_data()
+        self.df = pd.DataFrame.from_dict(self.data, orient="index")
+        self.df.index.name="key"
 
 
+    def copy_to_clipboard_by_input(self, target):
+        if (len(self.df) == 0):
+            print("data is empty")
+            return
+        if target:
+            key = target
+        else:
+            self.show_contents()
+            print("select a key to copy from", end=": ")
+            print(list(self.df.sort_values("timestamp").index))
+            key = input()
+        while(key not in self.data):
+            print("{} is not found in data. Select from".format(key), end=": ")
+            print(list(self.df.sort_values("timestamp").index))
+            key = input()
+        content = self.data[key]["content"]
+        pyperclip.copy(content)
+        print("Success: copied [{}]".format(content))
+
+    def delete_item_by_key(self, key):
+        if key in self.data:
+            del self.data[key]
+            f = open(self.json_data, "w")
+            json.dump(self.data, f)
+            print("\"{}\" is deleted".format(key))
+        else:
+            print("\"{}\" is not found in data".format(key))
+            exit()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="nothing")
-    parser.add_argument("--key", type=str)
-    parser.add_argument("--value", type=str)
-    parser.add_argument("--read", action="store_true")
-    parser.add_argument("--reset", action="store_true")
+    parser.add_argument("--clip", type=str)
+    parser.add_argument("-k", "--key", type=str)
+    parser.add_argument("-v", "--value", type=str)
+    parser.add_argument("-r", "--read", action="store_true")
+    parser.add_argument("--clear", action="store_true")
+    parser.add_argument("--delete", action="store_true")
+    parser.add_argument("-a", "--add", action="store_true")
     args = parser.parse_args()
 
     CBManager = ClipBoardManager()
 
+    if args.clip != None:
+        # CBManager.show_contents()
+        CBManager.copy_to_clipboard_by_input(args.clip)
+    
     if args.read:
         CBManager.show_contents()
+        # CBManager.copy_to_clipboard_by_input(args.clip)
         exit(0)
 
-    if args.reset:
+    if args.clear:
         CBManager.reset_contents()
+        CBManager.reload()
         CBManager.show_contents()
-        exit(0)
+        print("Success: cleared")
+        exit()
 
-    CBManager.add_data(args.key, args.value)
-    CBManager.show_contents()
+    if args.add:
+        CBManager.add_data(args.key, args.value)
+        CBManager.reload()
+        CBManager.show_contents()
+        exit()
     
+    if args.delete:
+        CBManager.delete_item_by_key(args.key)
+        CBManager.reload()
+        CBManager.show_contents()
+        exit()
 
