18c40a1b791b0db867cbdae1b680366b0acfa37a
Harukaze8 <uminohiyoko@gmail.com>
2020-01-02 14:43:00 +0900

    fix

--- clipboard/clipboard.py
+++ clipboard/clipboard.py
@@ -7,7 +7,7 @@ import pandas as pd #こいつのimport だけで0.5[s] かかる (これを外�
 import pyperclip
 import sys
 
-sys.path.append(os.path.dirname(__file__)+"/../")
+sys.path.append("..")
 import data_manager_base
 
 
